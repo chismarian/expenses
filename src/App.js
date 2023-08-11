11655aeae2822e156eeb071736d151d8b4dba377
chismarian <chis.marian25@gmail.com>
2023-08-11 15:08:29 +0300

    Expense Form added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -22,10 +23,17 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
+  let test = [];
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+
+    test = [...expenses, expense];
+    console.log(test);
+  };
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses expenses={expenses} />
     </div>
   );
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,21 @@
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeYear={filterChangeHandler}
+      />
       {props.expenses.map((item) => (
         <ExpenseItem
           key={item.id}
